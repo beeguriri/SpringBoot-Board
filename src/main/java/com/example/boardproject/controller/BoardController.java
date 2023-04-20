fa8bb7fbe14fb1b05ef34dbe866252c2d52f0745
Hyejin Kang <kkamboting@gmail.com>
2023-04-21 05:53:14 +0900

    0421_boardController및 board 관련 html작성

--- src/main/java/com/example/boardproject/controller/BoardController.java
+++ src/main/java/com/example/boardproject/controller/BoardController.java
@@ -1,11 +1,37 @@
 package com.example.boardproject.controller;
 
+import com.example.boardproject.domain.Board;
+import com.example.boardproject.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@Controller
 public class BoardController {
 
-    public void test() {
-        System.out.println("====> test");
+    @Autowired
+    BoardService boardService;
+
+    @GetMapping("/boardList")
+    public String getBoardList(Model model) {
+
+        List<Board> boardList = boardService.getBoardList();
+        model.addAttribute("boardList", boardList);
+
+        return "boardList";
+
     }
+
+    @GetMapping("/getBoard")
+    public String getBoard(Model model, Board board){
+
+        model.addAttribute("board", boardService.getBoard(board));
+
+        return "getBoard";
+    }
+
 }
