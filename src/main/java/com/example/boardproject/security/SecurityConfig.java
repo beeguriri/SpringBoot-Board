29b2ec39b9145927de9eafa1cec26de36b1c0a31
Hyejin Kang <kkamboting@gmail.com>
2023-04-23 21:54:54 +0900

    0423_security config 생성

---
+++ src/main/java/com/example/boardproject/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.boardproject.security;
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.net.http.HttpClient;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf().disable().cors().disable()
+                .authorizeHttpRequests(request -> request
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                        .anyRequest().authenticated()	// 어떠한 요청이라도 인증필요
+                )
+                .formLogin(login -> login	// form 방식 로그인 사용
+                        .defaultSuccessUrl("/boardList", true)	// 성공 시 boardList로
+                        .permitAll()
+                )
+                .logout(withDefaults());	// 로그아웃은 기본설정으로 (/logout으로 인증해제)
+
+        return http.build();
+    }
+}
