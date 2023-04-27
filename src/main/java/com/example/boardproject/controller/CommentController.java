d9b7acf57337235e9d1bcb70c5d294916770c226
Hyejin Kang <kkamboting@gmail.com>
2023-04-27 22:40:53 +0900

    0427_comment 입력 작업중

--- src/main/java/com/example/boardproject/controller/CommentController.java
+++ src/main/java/com/example/boardproject/controller/CommentController.java
@@ -1,7 +1,29 @@
 package com.example.boardproject.controller;
 
+import com.example.boardproject.domain.Board;
+import com.example.boardproject.domain.Comment;
+import com.example.boardproject.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class CommentController {
+
+    @Autowired
+    CommentService commentService;
+
+    @PostMapping("/addComment")
+    public String addComment(@AuthenticationPrincipal User user, Board board, Comment comment, RedirectAttributes redirect){
+
+        System.out.println("controller : " + board.getSeq());
+        redirect.addAttribute("seq", board.getSeq());
+        commentService.addComment(user.getUsername(), board.getSeq(), comment);
+
+
+        return "redirect:getBoard?seq={seq}";
+    }
 }
--- src/main/java/com/example/boardproject/service/CommentService.java
+++ src/main/java/com/example/boardproject/service/CommentService.java
@@ -1,7 +1,11 @@
 package com.example.boardproject.service;
 
+import com.example.boardproject.domain.Board;
 import com.example.boardproject.domain.Comment;
+import com.example.boardproject.domain.Member;
+import com.example.boardproject.persistence.BoardRepository;
 import com.example.boardproject.persistence.CommentRepository;
+import com.example.boardproject.persistence.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +17,26 @@ public class CommentService {
     @Autowired
     CommentRepository commentRepo;
 
+    @Autowired
+    MemberRepository memberRepo;
+
+    @Autowired
+    BoardRepository boardRepo;
+
     public List<Comment> getCommentList() {
 
         return (List<Comment>) commentRepo.findAll();
 
     }
 
-    public void addComment(Comment comment) {
+    public void addComment(String userId, Long boardSeq, Comment comment) {
 
+        System.out.println("service : " + boardSeq);
+
+        Member member = memberRepo.findByMemberId(userId).get();
+        Board getBoard = boardRepo.findById(boardSeq).get();
+        comment.setMember(member);
+        comment.setBoard(getBoard);
         commentRepo.save(comment);
 
     }
